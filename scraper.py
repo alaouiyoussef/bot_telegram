3fbdb0a852b3c569b55bea1efe0e2c6cb48b54f1
alaouiyoussef <ya6247111@gmail.com>
2026-03-17 01:43:39 +0100

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -13,14 +13,25 @@ from bs4 import BeautifulSoup
  
 URL = "https://www.marchespublics.gov.ma/index.php?page=entreprise.EntrepriseAdvancedSearch&searchAnnCons"
  
- 
 def start_driver():
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options,
-    )
-    driver.maximize_window()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    if os.environ.get("RAILWAY_ENVIRONMENT"):        # Sur Railway
+        options.binary_location = "/usr/bin/chromium"
+        driver = webdriver.Chrome(
+            service=Service("/usr/bin/chromedriver"),
+            options=options,
+        )
+    else:                                            # Sur ton PC local
+        driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()),
+            options=options,
+        )
     return driver
  
  
